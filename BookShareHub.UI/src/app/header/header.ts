d02a201f84e5306491bf24bbe09fe270e77f7be7
Jhonny Marcelo <jhonny-marcelo@outlook.com>
2026-06-06 18:38:28 -0300

    feat(header): enhance navigation with dynamic items and improve layout

--- BookShareHub.UI/src/app/header/header.ts
+++ BookShareHub.UI/src/app/header/header.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { NavigationItem } from './navigation-item';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
@@ -19,5 +21,72 @@ export class Header {
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  get navItems(): NavigationItem[] {
+    const currentRoute = this.router.url;
+
+    if (this.authService.isLoggedIn()) {
+      return [
+        {
+          label: 'Books',
+          route: '/books',
+        },
+        {
+          label: 'Logout',
+          action: () => this.logout(),
+        },
+      ];
+    }
+
+    switch (currentRoute) {
+      case '/login':
+        return [
+          {
+            label: 'Books',
+            route: '/books',
+          },
+        ];
+
+      case '/register':
+        return [
+          {
+            label: 'Books',
+            route: '/books',
+          },
+        ];
+
+      case '/forgot-password':
+        return [
+          {
+            label: 'Books',
+            route: '/books',
+          },
+          {
+            label: 'Login',
+            route: '/login',
+          },
+          {
+            label: 'Register',
+            route: '/register',
+          },
+        ];
+
+      default:
+        return [
+          {
+            label: 'Books',
+            route: '/books',
+          },
+          {
+            label: 'Login',
+            route: '/login',
+          },
+          {
+            label: 'Register',
+            route: '/register',
+          },
+        ];
+    }
   }
 }
