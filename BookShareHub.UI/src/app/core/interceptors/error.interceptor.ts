f259700947b6f628ef484a859795d57cb128f4a9
Jhonny Marcelo <jhonny-marcelo@outlook.com>
2026-07-04 18:58:29 -0300

    refactor: update AuthService import paths across the application

--- BookShareHub.UI/src/app/core/interceptors/error.interceptor.ts
+++ BookShareHub.UI/src/app/core/interceptors/error.interceptor.ts
@@ -2,8 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
-
-import { AuthService } from '../../auth/auth.service';
+import { AuthService } from '../services/auth.service';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
--- BookShareHub.UI/src/app/header/header.ts
+++ BookShareHub.UI/src/app/header/header.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { AuthService } from '../auth/auth.service';
+import { AuthService } from '../core/services/auth.service';
 import { Router } from '@angular/router';
 import { RouterLink } from '@angular/router';
 import { NavigationItem } from './navigation-item';
